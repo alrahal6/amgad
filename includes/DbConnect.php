7b4c3d67e1748a9e52640e950dfebc0072332234
sd <dhayal.cluzter@gmail.com>
2020-07-19 21:16:00 +0530

    connection string

--- includes/DbConnect.php
+++ includes/DbConnect.php
@@ -14,3 +14,27 @@ class DbConnect
         return $this->con;
     }
 }
+/*class DbConnect
+{
+    private $con;
+    function __construct() { }
+    function connect()
+    {
+        include_once dirname(__FILE__) . '/Info.php';
+        //$this->con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        $this->con = new mysqli(null,
+            'root', // username
+            'Prem@2512',     // password
+            'Amgad',
+            null,
+            '/cloudsql/al-rahal6:us-central1:root'
+            );
+        //set_charset($this->con,"UTF8");
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect with database" . mysqli_connect_error();
+        }
+        //var_dump($this->con);
+        //echo "connected Successfully";
+        return $this->con;
+    }
+}*/

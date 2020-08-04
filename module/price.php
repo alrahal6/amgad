aaa61fe16d0aea074dbc5c7077ac05e832e79cfc
sd <dhayal.cluzter@gmail.com>
2020-08-04 21:29:19 +0530

    revised price formula

--- module/price.php
+++ module/price.php
@@ -6,12 +6,48 @@ $response = array();
 function calculatePrice($distance,$flag) {
     $np = 0;
     //$dist = value.distance;
+    switch ($flag) {
+        case "1":
+            $np = getTaxiPrice($distance); 
+            break;
+        case "2":
+            $np = getPoolPrice($distance);
+            break;
+        case "3":
+            $np = 0; 
+            break;
+        case "4":
+            $np = getTaxiPrice($distance); 
+            break;
+        case "5":
+            $np = getPoolPrice($distance);
+            break;
+        case "6":
+            $np = 0;
+            break;
+        default:
+            $np = 0; 
+    }
+    /*if($distance > 3) {
+        $np = ((($distance - 3) * 17.6) + 70 + 30 );
+    } else {
+        $np = 70 + 30;
+    }*/ 
+    return $np; 
+}
+
+function getTaxiPrice($distance) {
+    $np = 0;
     if($distance > 3) {
         $np = ((($distance - 3) * 17.6) + 70 + 30 );
     } else {
         $np = 70 + 30;
     }
-    return $np; 
+    return $np;
+}
+
+function getPoolPrice($distance) {
+    return ($distance * 11) + 16.5; 
 }
 
 if($_SERVER['REQUEST_METHOD']=='POST') { 

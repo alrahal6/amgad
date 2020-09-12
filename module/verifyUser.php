6ee4d7c0ce942040798ceac7f7e21a96670a794c
sd <dhayal.cluzter@gmail.com>
2020-09-12 14:40:30 +0530

    verify user by selector

--- module/verifyUser.php
+++ module/verifyUser.php
@@ -10,12 +10,19 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
 		//if($user = $db->isValidUser($data['phone'], $data['password'])) {
 		    //$user = $db->getUserByPhone($_POST['phone']);
 		    
-        $response = array(
-            'userPhone' => $data['phone'],
-            'userId' => $data['userId'],
-            'isAllowed' => true,
-            'message' => 'User Allowed'
-        );
+        if($data['selector'] == 5) {
+            $response['userPhone'] = $data['phone'];
+            $response['userId'] = $data['userId'];
+            $response['isAllowed'] = false;
+            $response['message'] = "Blocked due to non payment";
+        } else {  
+            $response = array(
+                'userPhone' => $data['phone'],
+                'userId' => $data['userId'],
+                'isAllowed' => true,
+                'message' => 'User Allowed'
+            );
+        }
 		    /*$response['userPhone'] = $data['phone']; 
 			$response['userId'] = $data['userId'];
 			$response['isAllowed'] = true;

1bd3fe24811473e8dab750682ba9fbb6e650bfa4
sd <dhayal.cluzter@gmail.com>
2020-09-12 13:02:27 +0530

    verify user

--- module/verifyUser.php
+++ module/verifyUser.php
@@ -5,13 +5,30 @@ $response = array();
 
 if($_SERVER['REQUEST_METHOD']=='POST') { 
     $data = json_decode(file_get_contents('php://input'), true);
-    if(isset($data['userPhone']) and isset($data['userId'])){
+    if(isset($data['userId']) and isset($data['phone'])){
 		//$db = new DbOperations(); 
 		//if($user = $db->isValidUser($data['phone'], $data['password'])) {
 		    //$user = $db->getUserByPhone($_POST['phone']);
-		    $response['userPhone'] = $data['userPhone']; 
+		    
+        $response = array(
+            'userPhone' => $data['phone'],
+            'userId' => $data['userId'],
+            'isAllowed' => true,
+            'message' => 'User Allowed'
+        );
+		    /*$response['userPhone'] = $data['phone']; 
 			$response['userId'] = $data['userId'];
 			$response['isAllowed'] = true;
+			$response['message'] = "User Allowed";*/
+			
+			
+			
+			/*$response['userPhone'] = $data['userPhone'];
+			$response['userId'] = $data['userId'];
+			$response['isAllowed'] = false;
+			$response['message'] = "Blocked due to non payment";*/
+			
+			
 			//$response['password'] = "";
 			//$response['vehicleType'] = $user['vehicleType'];
 			//$db->insertLogin($user['id']);

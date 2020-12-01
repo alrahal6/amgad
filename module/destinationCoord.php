30c2c20b58e4953547ada16a9cbe8f102cf2bb9a
sd <dhayal.cluzter@gmail.com>
2020-12-01 18:18:18 +0530

    new files

---
+++ module/destinationCoord.php
@@ -0,0 +1,46 @@
+<?php 
+require_once dirname(__DIR__).'/includes/DbOperations.php';
+$response = array(); 
+if($_SERVER['REQUEST_METHOD']=='POST') { 
+    $data = json_decode(file_get_contents('php://input'), true);
+    //var_dump($data);
+    if(isset($data['userId']) and isset($data['lat']) and isset($data['lng']) ) { 
+        $db = new DbOperations(); 
+        $r = $db->setDestination($data['userId'],$data['lat'],$data['lng']);
+        //echo "after response";
+        //var_dump($response);
+        if($r == 1) {
+            $response = $data;
+            //$response['error'] = true;
+            //$response['message'] = "No Nearby Drivers found!";
+            /*$response[0] =
+            array(
+                'userId' => '0',
+                'distance' => '0',
+                'lat' => '00.00',
+                'lng' => '00.00', 
+            );*/ 
+        }  else { 
+            $response = $r;  
+        } 
+		//echo json_encode($array); 
+	} else {
+	    /*$response[0] =
+	    array(
+	        'userId' => '0',
+	        'distance' => '0',
+	        'lat' => '00.00',
+	        'lng' => '00.00',
+	    );*/
+	}
+} /*else {
+        $response[0] =
+            array(
+                
+                'driverId' => '0',
+                'distance' => '0',
+                'lat' => '00.00',
+                'lng' => '00.00',
+            ); 
+}*/ 
+echo json_encode($response);
